c98dc26e596e217e5382e5d241c2bb7a33da276e
simoncal-saas <simoncal@yahoo.com>
2017-08-08 12:39:50 -0700

    add form to home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,23 @@
 class VisitorsController < ApplicationController
 
   def new
-    flash[:notice] = 'Welcome!'
-    flash[:alert] = 'Access not allowed.'
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      flash.now[:notice] = "Chose #{@visitor.favorite}."
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:favorite, :comment)
   end
 
 end
